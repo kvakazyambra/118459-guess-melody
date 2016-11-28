0968d786d4c8f3e73f6a99cec619be885c539be1
chemist <chemist@design.ru>
2016-11-29 00:32:37 +0300

    Разделил данные и представление.

--- js/template-modules/main-level-genre.js
+++ js/template-modules/main-level-genre.js
@@ -3,34 +3,48 @@ import replaceContent from '../replace-content';
 import mainResult from './main-result';
 
 const moduleLevelGenre = function () {
+  const gameData = {
+    sliderHeader: 'Выберите инди-рок треки',
+    answers: [
+      {
+        id: 'a-1',
+        value: 'answer-1'
+      },
+      {
+        id: 'a-2',
+        value: 'answer-2'
+      },
+      {
+        id: 'a-3',
+        value: 'answer-3'
+      },
+      {
+        id: 'a-4',
+        value: 'answer-4'
+      }
+    ],
+    submitControlLabel: 'Ответить'
+  };
+
+  const header = `<h2 class="title">${gameData.sliderHeader}</h2>`;
+
+  let answers = '';
+
+  for (const answer of gameData.answers) {
+    answers += `<div class="genre-answer">
+        <div class="player-wrapper"></div>
+        <input type="checkbox" name="answer" value="${answer.value}" id="${answer.id}">
+        <label class="genre-answer-check" for="${answer.id}"></label>
+      </div>`;
+  }
+
+  const submitControlTpl = `<button class="genre-answer-send" type="submit">${gameData.submitControlLabel}</button>`;
+
   const block = getElementFromTemplate(`<section class="main main--level main--level-genre">
-      <h2 class="title">Выберите инди-рок треки</h2>
+      ${header}
       <form class="genre">
-        <div class="genre-answer">
-          <div class="player-wrapper"></div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-1">
-          <label class="genre-answer-check" for="a-1"></label>
-        </div>
-  
-        <div class="genre-answer">
-          <div class="player-wrapper"></div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-2">
-          <label class="genre-answer-check" for="a-2"></label>
-        </div>
-  
-        <div class="genre-answer">
-          <div class="player-wrapper"></div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-3">
-          <label class="genre-answer-check" for="a-3"></label>
-        </div>
-  
-        <div class="genre-answer">
-          <div class="player-wrapper"></div>
-          <input type="checkbox" name="answer" value="answer-1" id="a-4">
-          <label class="genre-answer-check" for="a-4"></label>
-        </div>
-  
-        <button class="genre-answer-send" type="submit">Ответить</button>
+        ${answers}
+        ${submitControlTpl}
       </form>
     </section>`);
 
--- js/template-modules/main-result.js
+++ js/template-modules/main-result.js
@@ -4,13 +4,30 @@ import welcome from './main-welcome';
 
 
 const moduleResult = function () {
+  const gameData = {
+    gameName: 'Угадай мелодию',
+    sliderHeader: 'Вы настоящий меломан!',
+    gameResults: {
+      result: 'За&nbsp;2&nbsp;минуты<br>вы&nbsp;отгадали 4&nbsp;мелодии',
+      stats: 'Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков'
+    },
+    submitControlLabel: 'Сыграть ещё раз'
+  };
+
+  const header = `<section class="logo" title="${gameData.gameName}"><h1>${gameData.gameName}</h1></section>`;
+
+  const sliderHeader = `<h2 class="title">${gameData.sliderHeader}</h2>`;
+
+  const gameResults = `<div class="main-stat">${gameData.gameResults.result}</div>
+        <span class="main-comparison">${gameData.gameResults.stats}</span>`;
+
+  const submitControl = `<span role="button" tabindex="0" class="main-replay">${gameData.submitControlLabel}</span>`;
+
   const block = getElementFromTemplate(`<section class="main main--result">
-      <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-  
-      <h2 class="title">Вы настоящий меломан!</h2>
-      <div class="main-stat">За&nbsp;2&nbsp;минуты<br>вы&nbsp;отгадали 4&nbsp;мелодии</div>
-      <span class="main-comparison">Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков</span>
-      <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
+      ${header}
+      ${sliderHeader}
+      ${gameResults}
+      ${submitControl}
     </section>`);
 
 
